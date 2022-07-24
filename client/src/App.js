36605d65cff58918cf1e0418dcfdbc9d944898f0
Paul Marquardt <marqrdt@gmail.com>
2022-07-23 22:55:55 -0600

    working site with random image rotation.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
 import 'bootswatch/dist/flatly/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import { MDBFooter } from 'mdb-react-ui-kit';
-import React, {useState} from 'react';
+import {useState, useEffect} from 'react';
 import MainNavigation from './components/MainNavigation';
 import './App.css';
 
 function getRandomBGImage() {
-    return "bg-image-" + Math.floor( Math.random() * 6 + 2 ).toString().padStart(2, "0");
+    return "bg-image-" + Math.floor( Math.random() * 8 + 2 ).toString().padStart(2, "0");
 }
 
 function App() {
     const [appState, setAppState] = useState([]);
-    let classes = getRandomBGImage() + ' main w-100';
+    const [bgImageClass, setBgImageClass] = useState(getRandomBGImage() + ' main w-100');
+    const [updateInterval, setUpdateInterval] = useState(60000);
+    useEffect(() => {
+        const updateImage = setInterval(() => {
+            setBgImageClass(getRandomBGImage() + ' main w-100');
+            //console.log(bgImageClass);
+            setUpdateInterval( Math.floor( Math.random() * 60000 + 60000 ));
+        }, updateInterval);
+        return () => clearInterval(updateImage);
+    }, []);
+
     return (
         <Container>
-            <Container fluid className={ classes }>
+            <Container fluid className={ bgImageClass }>
                 <MainNavigation/>
             </Container>
             <Container id="content" className='content'/>
