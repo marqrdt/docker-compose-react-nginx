ed66a3da90a7625c13320808510e8975f3613c96
Paul Marquardt <marqrdt@gmail.com>
2022-09-09 17:03:20 -0600

    Mo better rendering of blog entries.

--- client/src/components/BlogEntries/index.js
+++ client/src/components/BlogEntries/index.js
@@ -1,39 +1,35 @@
-import React, {useState, Fragment} from 'react';
-import {
-    Container,
-    Row,
-    Col
-} from 'react-bootstrap';
+import React, {useEffect, useState, Fragment} from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Axios from "axios"; // Import Axios or use Fetch.
 import ContentProvider from "../../data/ContentProvider";
 import BlogPost from '../BlogPost';
 
-class BlogEntries extends React.Component {
 
-  componentDidMount(props) {
+const BlogEntries = (props) => {
 
-  }
+  const contentProvider = new ContentProvider();
+  const blogz = contentProvider.getBlogList();
 
-  render(props) {
-    const contentProvider = new ContentProvider();
-    let blogz = contentProvider.getBlogList();
-    let index = 0;
-    return(
-        <Container>Coming soon: <strong>More stuff out of MA' BIG FUCKING BRAIN.</strong></Container>
-    )
-  }
+  useEffect(() => {
+    blogz.map(entry => {
+      console.log("Blog with title '" + entry.title + "' has file: " + entry.file);
+      Axios(entry.file).then(res => {
+        console.log("Blog with title '" + entry.title + "' has contents: " + res.data);
+      })
+      //fileReader.readAsText(entry.file);
+      //fileReader.onload = () => {
+      //  console.log("Blog with title '" + entry.title + " has content: " + fileReader.contents);
+      //}
+    })
+  }, [blogz])
 
-  renderFinal(props) {
-    const contentProvider = new ContentProvider();
-    let blogz = contentProvider.getBlogList();
-    let index = 0;
-    return(
-        <Container>
-            {blogz.map(entry => {
-                return <BlogPost key={index++} entry={entry}/>;
-            })}
-        </Container>
-    )
-  }
+  return(
+      <Container>
+          {blogz.map(entry => {
+            <BlogPost title={entry.title} content={entry.content} date={entry.date}/>;
+          })}
+      </Container>
+  )
 }
 
 export default BlogEntries;
